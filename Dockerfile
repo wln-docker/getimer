4f738b14e69ffe196fe226284b1e83faa19c3a8a
xcy <mail@xiechaoyi.com>
2024-11-09 21:22:06 +0800

    commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+######## Build Source ########
+FROM ccr.ccs.tencentyun.com/wlniao/dotnet-sdk:8.0.0-alpine as publish
+COPY ./ /code
+WORKDIR /code
+RUN dotnet publish -o /dist --self-contained -c Release -p:PublishAot=true -p:PublishTrimmed=true
+
+
+######## Build Images ########
+FROM ccr.ccs.tencentyun.com/wlniao/aot:8.0-alpine as builder
+COPY --chmod=0777 --from=publish /dist/getimer /wln
+CMD getimer
---
+++ Program.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Wlniao;
+var cfg = XCore.Init(args);
+if (cfg != null)
+{
+    var urls = Wlniao.Config.GetSetting("URLS").SplitBy();
+    if (int.TryParse(Wlniao.Config.GetSetting("WaitTime", "5000"), out int wait) || wait < 0)
+    {
+        if (wait > 0)
+        {
+            var http = new Http { ServerName = "Getimer", ListenPort = cfg.GetInt32("Port", 80) };
+            http.Handler = new System.Action<Http.Context>((ctx) =>
+            {
+                if (ctx.Path.IndexOf('.') < 0)
+                {
+                    var finish = 0;
+                    var endTime = DateTime.Now.AddMilliseconds(wait);
+                    foreach (var url in urls)
+                    {
+                        new System.Threading.Thread(() =>
+                        {
+                            var time = DateTools.Format();
+                            var result = Wlniao.NetTools.Request(url, "");
+                            Console.WriteLine($"{time} {url} => {result}");
+                            finish++;
+                        }).Start();
+                    }
+                    while (finish < urls.Length && DateTime.Now < endTime)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                    ctx.Response = $"Finish: {finish}";
+                }
+            });
+            http.Start();
+        }
+        else
+        {
+            foreach (var url in urls)
+            {
+                var time = DateTools.Format();
+                var result = Wlniao.NetTools.Request(url, "");
+                Console.WriteLine($"{time} {url} => {result}");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("The parameter 'WaitTime' is invalid");
+    }
+}
